bb92606812dd72b505617bd7f0b0ced92d61dfd3
Dahmani Abdelmadjid <dahmadjid@gmail.com>
2022-01-24 23:00:37 +0100

    Update main.cpp

--- spi_master_slave/spi_master/main/main.cpp
+++ spi_master_slave/spi_master/main/main.cpp
@@ -1,25 +1,12 @@
-/* C++ exception handling example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include <iostream>
 #include <string.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/semphr.h"
-#include "freertos/queue.h"
-#include "driver/gpio.h"
-#include "driver/spi_slave.h"
+
 #include "driver/spi_master.h"
+
 #include "esp_log.h"
-
-
-
 
 #define GPIO_MISO GPIO_NUM_19
 #define GPIO_MOSI GPIO_NUM_23
@@ -55,94 +42,31 @@ void spiMasterInit(void)
     spi_bus_add_device(RCV_HOST, &devcfg, &spi);
 }
 
-void spiSlaveInit(void)
-{
-    spi_bus_config_t buscfg = {0};  
-    
-    buscfg.mosi_io_num = GPIO_NUM_13;
-    buscfg.miso_io_num = GPIO_NUM_12;
-    buscfg.sclk_io_num = GPIO_NUM_14;
-    buscfg.quadwp_io_num = -1;
-    buscfg.quadhd_io_num = -1;
-    
-    spi_slave_interface_config_t slvcfg = {0};
-    slvcfg.mode = 0;
-    slvcfg.spics_io_num = GPIO_NUM_27;
-    slvcfg.queue_size = 3;
-    slvcfg.flags = 0;
-    
-
-    gpio_set_pull_mode(GPIO_NUM_13, GPIO_PULLUP_ONLY);
-    gpio_set_pull_mode(GPIO_NUM_14, GPIO_PULLUP_ONLY);
-    gpio_set_pull_mode(GPIO_NUM_27, GPIO_PULLUP_ONLY);
-    spi_slave_initialize(HSPI_HOST, &buscfg, &slvcfg, NULL);
-}
-
-// extern "C" void app_main(void)
-// {
-//     spiMasterInit();
-//     uint8_t n = 255;
-//     char sendbuf[64]= {0};
-//     char recvbuf[64]= {0};
-//     while(1)
-//     {
-        
-//         spi_transaction_t t = {0};
-//         snprintf(sendbuf, 64, "Hello from master esp32: %d", n);
-//         t.length = 8*64;
-//         t.tx_buffer = sendbuf;
-//         t.rx_buffer = recvbuf;
-        
-        
-//         ret = spi_device_transmit(spi, &t);
-//         if (ret != ESP_OK)
-//         {
-//             ESP_LOGE("SPI_MASTER", "Failed to transmit: %s", esp_err_to_name(ret));
-//         }
-
-//         ESP_LOGI("SPI_MASTER", "Transmission is successful: sent: %s, received: %s", sendbuf, recvbuf);
-//         vTaskDelay(2000/portTICK_PERIOD_MS);
-//         n--;
-//     }   
-// }
-
 extern "C" void app_main(void)
 {
-    spiSlaveInit();
-    // gpio_set_direction(GPIO_NUM_27, GPIO_MODE_INPUT);
-    // gpio_pad_select_gpio(GPIO_NUM_27);
-    // gpio_set_pull_mode(GPIO_NUM_27, GPIO_PULLUP_ONLY);
-    // while(1) 
-    // {
-    //     int i = gpio_get_level(GPIO_NUM_27);
-    //     ESP_LOGI("gpio 27:", "%d", i);
-    //     vTaskDelay(1);
-
-    // }
-    // Configuration for the SPI bus
-    
-    uint8_t n = 0;
+    spiMasterInit();
+   
+    uint8_t n = 255;
     char sendbuf[1]= {0};
     char recvbuf[1]= {0};
-
-    while(1) 
+   
+    while(1)
     {
-        spi_slave_transaction_t t = {0};
         
-        t.length = 8;
+        spi_transaction_t t = {0};
+        snprintf(sendbuf, 64, "Hello from master esp32: %d", n);
+        t.length = 8*64;
         t.tx_buffer = sendbuf;
         t.rx_buffer = recvbuf;
-        sendbuf[0] = n;
-        ret = spi_slave_transmit(SPI2_HOST, &t, portMAX_DELAY);
+       
+        ret = spi_device_transmit(spi, &t);
         if (ret != ESP_OK)
         {
-            ESP_LOGE("SPI_SLAVE","Failed to transmit: %s", esp_err_to_name(ret));
-            continue;
+            ESP_LOGE("SPI_MASTER", "Failed to transmit: %s", esp_err_to_name(ret));
         }
 
-        ESP_LOGI("SPI_SLAVE","Transmission is successful: received: %d, sent: %d", recvbuf[0], sendbuf[0]);
-        
-        n++;  
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-    }
+        ESP_LOGI("SPI_MASTER", "Transmission is successful: sent: %d, received: %d", sendbuf[0], recvbuf[0]);
+        vTaskDelay(2000/portTICK_PERIOD_MS);
+        n--;
+    }   
 }
