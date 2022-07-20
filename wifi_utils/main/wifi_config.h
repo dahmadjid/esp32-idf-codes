2e3a91436cd2b0652384efbe6fda7548e932fa3c
Dahmani Abdelmadjid <dahmadjid@gmail.com>
2022-07-20 22:51:44 +0200

    Update wifi_config.h

--- wifi_utils/main/wifi_config.h
+++ wifi_utils/main/wifi_config.h
@@ -1,3 +1,7 @@
+#ifndef _WIFI_CONFIG_H_
+#define _WIFI_CONFIG_H_
+
+
 #define STA_SSID "router_wifi"
 #define STA_PASSWORD "12345678"
 #define STA_MAX_RETRY 5
@@ -6,3 +10,6 @@
 #define AP_SSID "espwifi"
 #define AP_PASSWORD "12345678" 
 #define AP_MAX_CONNECTIONS 5   // Max 10 
+
+
+#endif
